9373d30c25011585d96c548bc9a02f9c8fc6b2f1
Yashuvk18 <125733880+Yashuvk18@users.noreply.github.com>
2025-04-16 10:03:32 +0530

    app.jsx

---
+++ app.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Checkout from './pages/Checkout';
+import Success from './pages/Success';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/success" element={<Success />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
